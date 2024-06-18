058b8e057c1043f85a08a1b93463e9011ece7fdb
dingo <chatziva@protonmail.com>
2024-06-18 09:25:36 +0100

    consolidate

--- game.py
+++ game.py
@@ -1,61 +1,90 @@
 import random
-from ui import UI
+import os
 
-DICTIONARY = "dictionary.txt"
+DICTIONARY = "words.txt"
+VALID_LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+NB_GUESSES = 10
 
 
-class Hangman:
-    def __init__(self, ui: UI, nb_guesses: int, with_word: str = "") -> None:
-        self.ui = ui
-        self.guesses_left = nb_guesses
-        self.incorrect_guesses = []
-        self.correct_guesses = []
-        self.secret_word = with_word.upper() or self.choose_random_word()
 
-    @staticmethod
-    def choose_random_word() -> str:
-        words = []
-        with open(DICTIONARY) as f:
-            for line in f:
-                word = line.strip().split("'")[0]
-                if len(word) > 3 and len(set(word)) > 1 and word.capitalize() == word:
-                    words.append(word.upper())
-        return random.choice(words)
+def get_letter() -> str:
+    while True:
+        guess = input("Guess a letter: ")
+        if len(guess) == 1:
+            if guess in VALID_LETTERS.upper() or guess in VALID_LETTERS.lower():
+                return guess.upper()
+        print("Invalid guess... Use letters only.")
 
-    @property
-    def nb_letters_found(self) -> int:
-        return sum(self.secret_word.count(letter) for letter in self.correct_guesses)
 
-    @property
-    def revealed_word(self) -> str:
-        revealed = ""
-        for letter in self.secret_word:
-            if letter in self.correct_guesses:
-                revealed += letter + " "
-            else:
-                revealed += "_" + " "
-        return revealed
+def clear_screen() -> None:
+    os.system("clear")
 
-    @property
-    def all_letters_found(self) -> bool:
-        return self.nb_letters_found == len(self.secret_word)
 
-    def run(self) -> None:
-        while True:
-            self.ui.clear_screen()
-            self.ui.display_msg(self.revealed_word)
-            self.ui.display_round_msg(self.guesses_left, self.incorrect_guesses)
-            guess = self.ui.get_letter()
-            if guess in self.secret_word:
-                if guess not in self.correct_guesses:
-                    self.correct_guesses.append(guess)
-                if self.all_letters_found:
-                    self.ui.display_win_msg()
-                    return
-            else:
-                if guess not in self.incorrect_guesses:
-                    self.incorrect_guesses.append(guess)
-                    self.guesses_left -= 1
-                if self.guesses_left == 0:
-                    self.ui.display_lost_msg(self.secret_word)
-                    return
+def display_round_msg(guesses_left: int, incorrect_guesses: list[str]) -> None:
+    print(
+        f"\n{guesses_left} guesses left. Already guessed: {', '.join(incorrect_guesses)}\n"
+    )
+
+
+def display_win_msg() -> None:
+    print("\nCongrats, you found the word! You won!\n")
+
+
+def display_lost_msg(word: str) -> None:
+    print(f"\nNo guesses left... Game over... The word was {word}.\n")
+
+
+def choose_random_word() -> str:
+    words = []
+    with open(DICTIONARY) as f:
+        for line in f:
+            word = line.strip().split("'")[0]
+            if len(word) > 3 and len(set(word)) > 1 and word.capitalize() == word:
+                words.append(word.upper())
+    return random.choice(words)
+
+
+def nb_letters_found(self) -> int:
+    return sum(self.secret_word.count(letter) for letter in self.correct_guesses)
+
+
+def reveal_word(secret_word: str, correct_guesses: list[str]) -> str:
+    revealed = ""
+    for letter in secret_word:
+        if letter in correct_guesses:
+            revealed += letter + " "
+        else:
+            revealed += "_" + " "
+    return revealed
+
+
+def all_letters_found(self) -> bool:
+    return self.nb_letters_found == len(self.secret_word)
+
+
+def run(with_word) -> None:
+
+    secret_word = choose_random_word()
+    guesses_left = NB_GUESSES
+    correct_guesses = []
+    incorrect_guesses = []
+
+    while True:
+        clear_screen()
+
+        reveal_word(secret_word=secret_word, correct_guesses=correct_guesses)
+        display_round_msg(guesses_left, incorrect_guesses)
+        guess = get_letter()
+        if guess in secret_word:
+            if guess not in correct_guesses:
+                correct_guesses.append(guess)
+            if all_letters_found:
+                display_win_msg()
+                return
+        else:
+            if guess not in incorrect_guesses:
+                incorrect_guesses.append(guess)
+                guesses_left -= 1
+            if guesses_left == 0:
+                display_lost_msg(secret_word)
+                return
